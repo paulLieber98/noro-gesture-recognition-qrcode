6d3d37193d94771b8dcbf83716d3883b4fb713f4
paulLieber98 <paullieber07@gmail.com>
2025-07-23 15:43:37 -0700

    index finger can now activate qr code

--- gesture_qr.py
+++ gesture_qr.py
@@ -3,6 +3,8 @@
 #used this for hand detection: https://www.youtube.com/watch?v=RRBXVu5UE-U
 #                              https://ai.google.dev/edge/mediapipe/solutions/vision/hand_landmarker
 
+#other: https://www.geeksforgeeks.org/python/python-opencv-cv2-cvtcolor-method/
+
 #ACTING AS THE WEBCAMS FROM NORO SCREEN
 import cv2 as cv
 
@@ -15,6 +17,10 @@ from mediapipe.tasks.python.vision import RunningMode
 from mediapipe.tasks.python.core import base_options
 
 import time #to make QR code popup temporary
+
+import qrcode
+
+import numpy as np
  
 
 #init webcam class
@@ -29,7 +35,16 @@ if not webcam.isOpened(): #if camera is not opened --> exit
 #defining QR code things
 qrcode_is_shown = False #starts as qrcode hidden
 qrcode_shown_start_time = 0 #starts at 0 seconds. going to last for 15 seconds (can be changed obviously)
+should_show_qrcode = False #starts as False. going to be True if index finger is up
 
+def generate_qr():
+    qr = qrcode.make('https://www.noro.co/') #replace link with whatever the actual link is for the qrcode-Noro-screen remote
+    qr_converted = qr.convert('RGB') 
+    qr_array = cv.cvtColor(np.array(qr_converted), cv.COLOR_RGB2BGR) #convert to opencv image: takes only numpy array + BGR
+    return qr_array
+
+
+#DEFINING MODEL
 
 #initializing hand detection things
 # mp_hands = mp.solutions.hands
@@ -49,10 +64,9 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 #     min_tracking_confidence=0.5 #random value. dont know what it does exactly
 # )
 
-
-
 # Create a hand landmarker instance with the live stream mode:
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+
 
     #actual hand detection process
     if result.hand_landmarks: #if at least 1 hand is detected:
@@ -61,14 +75,37 @@ def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp
             #print(result.multi_hand_landmarks)
             # mp_drawing.draw_landmarks(new_RBG_frame, hand_landmarks, mp_hands.HAND_CONNECTIONS) #draws the green/red dots/lines on the hand
     
-            #checking if index finger is up
-            if hand_landmarks[8].y < hand_landmarks[5].y: #[8] is the index finger tip, 
-                #[5] is the index finger bottom. #also, point 0,0 is top left corner so thats why '<' and not '>'
-                
+            #checking if index finger is only finger up
+            if hand_landmarks[8].y < hand_landmarks[5].y and hand_landmarks[12].y > hand_landmarks[9].y and hand_landmarks[16].y > hand_landmarks[13].y and hand_landmarks[20].y > hand_landmarks[17].y: 
+                #[8] is the index finger tip. [5] is the index finger bottom. 
+                # #also, point 0,0 is top left corner so thats why '<' and not '>'
                 #if index tip is higher than bottom, then index finger is up
-                print("Index finger is up")
 
-                '''DO QR CODE POPUP HERE'''
+                #'and' statements are to make sure that the other fingers are down. 
+
+                # print("Index finger is up")
+
+                #qrcode popup
+                # global qrcode_is_shown
+
+                global should_show_qrcode, qrcode_is_shown
+
+                if qrcode_is_shown == False: #if qrcode is not shown, then show it
+                    qrcode_is_shown = True
+                    qrcode_shown_start_time = time.time()
+                    # qrcode = generate_qr()
+                    # cv.imshow('QR Code', qrcode)
+                    should_show_qrcode = True
+                
+                # global qrcode_is_shown
+
+                #temporary popup for qrcode
+                # if qrcode_is_shown and time.time() - qrcode_shown_start_time >= 15: #time.time() = current time - the start time gives 
+                #     #us the total time qrcode has been shown
+                #     qrcode_is_shown = False
+                #     qrcode_shown_start_time = 0
+                #     cv.destroyWindow('QR Code')
+
 
 
 #params for hand detection model
@@ -77,6 +114,9 @@ options = HandLandmarkerOptions(
     running_mode=VisionRunningMode.LIVE_STREAM, #using live stream mode since its going to be real time feed during calls
     result_callback=print_result,
     num_hands=50) #50 hands max scannable (can be changed). thinking of it like in a conference room. Up to 25 people
+
+
+# ACTUAL MODEL IN USE
 
 #actually init hand detection model
 with HandLandmarker.create_from_options(options) as landmarker:
@@ -95,10 +135,32 @@ with HandLandmarker.create_from_options(options) as landmarker:
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=new_RBG_frame) #doing this bc mediapipe doesn't use numpy frames like opencv does
             #mp.ImageFormat.SRGB = 'RBG' simply
 
+
+
         #real time feed so we need to give the timestamp of each individual frame since were using 'live stream' mode
         timestamp_ms = int(time.time() * 1000) #timestamp in milliseconds
         landmarker.detect_async(mp_image, timestamp_ms) # processes and detects hands in video frame
 
+
+
+        #displaying qrcode if it should be shown
+        if should_show_qrcode:
+            qrcode_is_shown = True
+            qrcode_shown_start_time = time.time()
+            qrcode = generate_qr()
+            cv.imshow('QR Code', qrcode)
+            print("QR Code is being shown")
+            should_show_qrcode = False #resetting to False so that it doesnt show again
+
+        #temporary popup for qrcode
+        if qrcode_is_shown and time.time() - qrcode_shown_start_time >= 15: 
+            #time.time() = current time - the start time gives us the total time qrcode has been shown
+            qrcode_is_shown = False
+            qrcode_shown_start_time = 0
+            cv.destroyWindow('QR Code')
+
+            
+            
 
         #actual hand detection process
         # result = hand.detect_async(new_RBG_frame)
@@ -114,7 +176,6 @@ with HandLandmarker.create_from_options(options) as landmarker:
                     
         #             #if index tip is higher than bottom, then index finger is up
         #             print("Index finger is up")
-
 
 
         #displaying frames in a window
