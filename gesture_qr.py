8dcd1543f93980bf41b3e43565ca223c5d90afd6
paulLieber98 <paullieber07@gmail.com>
2025-08-06 18:55:02 -0700

    renamed result callback functions for hand detection and segmentation for clarity

--- gesture_qr.py
+++ gesture_qr.py
@@ -27,7 +27,6 @@ from typing import List
 from mediapipe import Image
 from mediapipe.tasks.python.vision import ImageSegmenter, ImageSegmenterOptions
 
-
 import time #to make QR code popup temporary
 
 import qrcode
@@ -53,7 +52,7 @@ def generate_qr():
 
 #DEFINING HAND DETECTION MODEL
 
-# BaseOptions = mp.tasks.BaseOptions
+
 BaseOptions = base_options.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
@@ -62,8 +61,7 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 
 # Create a hand landmarker instance with the live stream mode:
-def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-
+def print_result_hand_detection(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
 
     #actual hand detection process
     if result.hand_landmarks: #if at least 1 hand is detected:
@@ -108,7 +106,7 @@ def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp
 options_hand_detection = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='hand_landmarker.task'), #hand detection model in root dir
     running_mode=VisionRunningMode.LIVE_STREAM, #using live stream mode since its going to be real time feed during calls
-    result_callback=print_result,
+    result_callback=print_result_hand_detection,
     num_hands=50) #50 hands max scannable (can be changed). thinking of it like in a conference room. Up to 25 people
 
 
@@ -123,23 +121,23 @@ ImageSegmenterOptions = mp.tasks.vision.ImageSegmenterOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 # Create a image segmenter instance with the live stream mode:
-def print_result(result: List[Image], output_image: Image, timestamp_ms: int):
+def print_result_segmentation(result: List[Image], output_image: Image, timestamp_ms: int):
     print('segmented masks size: {}'.format(len(result)))
 
 options_segmentation = ImageSegmenterOptions(
     base_options=BaseOptions(model_asset_path='selfie_segmenter.tflite'),
     running_mode=VisionRunningMode.VIDEO,
     output_category_mask=True)
-# with ImageSegmenter.create_from_options(options) as segmenter:
     
 
-# ACTUAL MODEL IN USE
+
+# ACTUAL MODELS IN USE
 
 #actually init hand detection model
 with HandLandmarker.create_from_options(options_hand_detection) as landmarker:
     with ImageSegmenter.create_from_options(options_segmentation) as segmenter:
 
-            #MAIN CAMERA LOOP
+        #MAIN CAMERA LOOP
         while True: #camera on until user presses 'q'
 
             ret, frame = webcam.read() #ret: boolean value(True if camera gives a frame, False if not)
@@ -186,53 +184,7 @@ with HandLandmarker.create_from_options(options_hand_detection) as landmarker:
             if cv.waitKey(1) == ord('q'): #exit if user presses 'q'
                 break
 
-    # #MAIN CAMERA LOOP
-    # while True: #camera on until user presses 'q'
-
-    #     ret, frame = webcam.read() #ret: boolean value(True if camera gives a frame, False if not)
-    #     # frame: the actual individual frame from the video that were seeing ?
-    
-    #     if not ret: #if camera didn't give a frame --> exit
-    #         print("Can't receive frame (stream end?). Exiting ...")
-    #         break
-        
-
-    #     # opencv uses BGR, mediapipe uses RGB. fixing order from BGR to RGB:
-    #     new_RBG_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    #     #convert opencv image to mediapipe image
-    #     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=new_RBG_frame) #doing this bc mediapipe doesn't use numpy frames like opencv does
-    #         #mp.ImageFormat.SRGB is the same thing as 'RBG' simply
-
-
-    #     #real time feed so we need to give the timestamp of each individual frame since were using 'live stream' mode
-    #     timestamp_ms = int(time.time() * 1000) #timestamp in milliseconds
-    #     landmarker.detect_async(mp_image, timestamp_ms) # processes and detects hands in video frame
-
-    #     #  to get rid of blue-ish color lens, we need to convert back to BGR
-    #     final_frame = cv.cvtColor(new_RBG_frame, cv.COLOR_RGB2BGR)
-    #     #displaying frames in a window
-    #     cv.imshow('frame', final_frame) #displays frames in a window(thats what imshow does: opens a new window)
-
-
-    #     #displaying qrcode if it should be shown
-    #     if should_show_qrcode:
-    #         qrcode_is_shown = True
-    #         qrcode_shown_start_time = time.time()
-    #         qrcode_generated = generate_qr()
-    #         cv.imshow('QR Code', qrcode_generated)
-    #         print("QR Code is being shown")
-    #         should_show_qrcode = False #resetting to False so that it doesnt show again
-
-    #     #temporary popup for qrcode
-    #     if qrcode_is_shown and time.time() - qrcode_shown_start_time >= 10: # 10 seconds is the time qrcode is shown for
-    #         #time.time() = current time - the start time gives us the total time qrcode has been shown
-    #         qrcode_is_shown = False
-    #         qrcode_shown_start_time = 0
-    #         cv.destroyWindow('QR Code')
-
-    #     if cv.waitKey(1) == ord('q'): #exit if user presses 'q'
-    #         break
-
+   
 webcam.release()
 
 cv.destroyAllWindows()
